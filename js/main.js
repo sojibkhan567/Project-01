d4751dab37b432ec9b1fe01bae8fd64b808d8acf
sojibkhan567 <63386399+sojibkhan567@users.noreply.github.com>
2022-01-28 19:28:22 +0600

    First Upload
    add a new file

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Veriable coerction
+var fristName = "Sojib";
+var lastName = "Khan";
+
+var job, isMarried, age;
+job = "Web Designer";
+age = 25;
+isMarried = "false";
+
+console.log(fristName + ' ' + lastName + ' is a ' + age + " years old " + job + "." + "Is he married? " +isMarried);
+
+//Veriable Mutattion
+//alert(fristName + ' ' + lastName + ' is a ' + age + " years old " + job + "." + "Is he married? " +isMarried);
+
+//var lastName = prompt("what is his last name ?");
+
+console.log(fristName + ' ' + lastName);
+
+
